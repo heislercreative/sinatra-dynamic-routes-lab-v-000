5ddbe9bf80f8908f8af9e065d70d108354b1e1e5
Brennan Heisler <learn@heislercreative.com>
2018-03-16 22:23:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,6 +22,10 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
+    @op = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    
     
   end
   
