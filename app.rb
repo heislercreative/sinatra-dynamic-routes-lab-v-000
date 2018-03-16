82cf2a78394bc33ee7c716399dc4dbce7d46c2ee
Brennan Heisler <learn@heislercreative.com>
2018-03-16 22:26:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     @num1 = params[:number1]
     @num2 = params[:number2]
     
-    if @op = "add"
+    if @op = "add.to_downcase"
       answer = @num1 + @num2
       "answer"
     else
