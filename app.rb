5abaf8982f430f349714fa8638ded397d2da0d42
Brennan Heisler <learn@heislercreative.com>
2018-03-16 22:31:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     @op = params[:operation].to_downcase
-    @num1 = params[:number1]
-    @num2 = params[:number2]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
     
     case @op
       when "add"
